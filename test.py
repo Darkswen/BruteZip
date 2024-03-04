0635445b25c4af432847c735c951ef7b6c4b4518
Darkswen <darqswen@gmail.com>
2024-03-04 06:54:34 +0300

    проверяю Push

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-for i in range(1, 6):
-    print(f"Test {i}")
+import random
+
+# генерирует случайный пароль заданной длины из заданных символов
+pwd_chars='0123456789'
+pwd_len=8
+pwd=''
+for i in range(pwd_len):
+    pwd+=random.choice(pwd_chars)
+print(pwd)
