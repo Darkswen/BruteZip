08378bdc6c588826aea2b8a45f5595b14d0bbd4e
Darkswen <darqswen@gmail.com>
2024-03-04 15:01:42 +0300

    подключил DarkMint

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import random
 
 # генерирует случайный пароль заданной длины из заданных символов
 pwd_chars='0123456789'
-pwd_len=8
+pwd_len=6
 pwd=''
 for i in range(pwd_len):
     pwd+=random.choice(pwd_chars)
